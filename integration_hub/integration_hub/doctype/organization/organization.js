e143742a3cec4da23407080f1c78b9b32b4b1518
Vladislav Akashev <vlad.akashev.w@gmail.com>
2024-12-19 15:38:47 +0300

    feat: Add Doctype Organization

---
+++ integration_hub/integration_hub/doctype/organization/organization.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Vlad Akashev and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Organization", {
+// 	refresh(frm) {
+
+// 	},
+// });
