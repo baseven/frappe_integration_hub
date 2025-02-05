2c6aa2ae27f92859eab406d44fc30a97ef5b5b90
Vladislav Akashev <vlad.akashev.w@gmail.com>
2025-02-05 21:29:59 +0300

    feat: Add "Integration Flow Field" Doctype for storing selected fields
    - Created new table Doctype "Integration Flow Field" to store fields used in integration flows.

---
+++ integration_hub/integration_hub/doctype/integration_flow_field/integration_flow_field.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Vlad Akashev and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class IntegrationFlowField(Document):
+	pass
