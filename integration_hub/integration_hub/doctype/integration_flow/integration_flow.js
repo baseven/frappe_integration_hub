60136a4b643614f40ac955c421983c1cb838687d
Vladislav Akashev <vlad.akashev.w@gmail.com>
2025-02-05 21:30:56 +0300

    feat: Add "Integration Flow" Doctype for managing integration processes
    - Created a new Doctype "Integration Flow" to store integration configurations.
    - Added fields:
      - `flow_name`: Flow name (required).
      - `config_name`: Linked to "Integration Configuration".
      - `entity_type`: Stores selected EntityType.
      - `fields`: Table storing selected fields for integration.
      - `status`: Select field to track flow state (Draft, Active, Completed, Failed).
    - This Doctype will be used to manage integration flows between Frappe and external OData sources.

---
+++ integration_hub/integration_hub/doctype/integration_flow/integration_flow.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Vlad Akashev and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Integration Flow", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ integration_hub/integration_hub/doctype/integration_flow/test_integration_flow.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Vlad Akashev and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestIntegrationFlow(FrappeTestCase):
+	pass
